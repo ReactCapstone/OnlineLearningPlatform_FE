5dc1817b72844722417bc6822cae5fc8926e988e
Shravani Kardekar <shravani.kardekar@newvision-software.com>
2026-07-08 10:43:07 +0530

    resolving merge conflicts:

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,4 @@
-
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import DashboardHome from '../pages/StudentDashboard/DashboardHome'
 import Home from '../pages/Home'
@@ -7,20 +6,18 @@ import Courses from '../pages/Courses'
 import AllCourses from '../pages/AllCourses'
 import Login from '../pages/Login'
 import Profile from '../pages/Profile'
+import Register from '../pages/Register'
 
 export default function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={<DashboardHome />} />
-<<<<<<< HEAD
-            <Route path="/home" element={<Home />} />
-=======
-            <Route path="/home" element={<Home />} /> 
->>>>>>> ec323c7ed7421bdd0b2f2cbd249c52304b8fd68e
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/all-courses" element={<AllCourses />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<DashboardHome />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/courses" element={<Courses />} />
+      <Route path="/all-courses" element={<AllCourses />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  )
 }
